e50d230f47af5f4257bdcb38862567fc117d97b3
Eli <buchereli@gmail.com>
2015-06-19 10:09:39 -0400

    Created "Where are you going?" screen

--- Lunchly/app/src/main/java/com/example/lunchly/MainActivity.java
+++ Lunchly/app/src/main/java/com/example/lunchly/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.lunchly;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void next(View v){
+
     }
 }

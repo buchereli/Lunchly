058ff8b9ff7e6d6a43f6367eb96eea75504fffa9
Eli <buchereli@gmail.com>
2015-06-19 11:38:57 -0400

    Added "Set Drop-off Time" screen

--- Lunchly/app/src/main/java/com/example/lunchly/FinalizeActivity.java
+++ Lunchly/app/src/main/java/com/example/lunchly/FinalizeActivity.java
@@ -1,12 +1,19 @@
 package com.example.lunchly;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.View;
 
 /**
  * Created by Eli on 6/19/2015.
  */
 public class FinalizeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finalize);
+    }
 
     public void finalize(View v){
 
---
+++ Lunchly/app/src/main/java/com/example/lunchly/TimeSelectActivity.java
@@ -0,0 +1,10 @@
+package com.example.lunchly;
+
+import android.app.Activity;
+
+/**
+ * Created by Eli on 6/19/2015.
+ */
+public class TimeSelectActivity extends Activity {
+
+}

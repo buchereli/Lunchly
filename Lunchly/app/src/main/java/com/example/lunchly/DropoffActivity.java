dbe3b12a5706bcdbca35f771efc0b12dd87c7541
buchereli <buchereli@gmail.com>
2015-08-27 12:44:13 -0400

    Fixed Focus Problem

--- Lunchly/app/src/main/java/com/example/lunchly/DropoffActivity.java
+++ Lunchly/app/src/main/java/com/example/lunchly/DropoffActivity.java
@@ -10,6 +10,9 @@ public class DropoffActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dropoff);
+
+        GoogleMap googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+
     }
 
     public void setDropoffTime(View v){
